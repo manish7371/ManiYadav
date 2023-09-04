c26cd8471576b2122a73358137f486ed6e51bb73
Manish Yadav <138249714+manish7371@users.noreply.github.com>
2023-09-04 18:10:51 +0530

    Bootstrap Using Site

---
+++ boots.js
@@ -0,0 +1,24 @@
+
+// function closefun(){
+// window.close()
+// }
+
+// window.addEventListener('load' ,function openwindow() {
+//     this.alert('You Are 18+')
+// })
+// openwindow()
+
+// function openwindows() {
+//     alert('Are You 18+')
+// }
+
+
+// let myget=document.getElementById('nav').getAttribute('class');
+// console.log(myget);
+
+
+// let myvar=document.getElementById('nav').innerHTML;
+// console.log(myvar);
+
+// let m = document.getElementById('nav').removeAttribute('class');
+// console.log(m);
